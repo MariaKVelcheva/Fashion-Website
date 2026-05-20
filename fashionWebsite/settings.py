f52c274bb7e81708c47134359cc87c036529211d
MariaKVelcheva <maria.k.velcheva@gmail.com>
2026-05-20 15:37:31 +0200

    fix: database fix

--- fashionWebsite/settings.py
+++ fashionWebsite/settings.py
@@ -119,7 +119,7 @@ WSGI_APPLICATION = 'fashionWebsite.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        env="DATABASE_URL",
+        env="DATABASE_PUBLIC_URL",
         default="postgresql://postgres:new_password@localhost:5432/fashion_db",
         conn_max_age=600,
     )
